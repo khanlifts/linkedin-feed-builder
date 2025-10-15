46a56809b7be4f57f9039eb2268c17a1a647d2fa
Cyril Khan <cyrilkhan@users.noreply.github.com>
2025-10-15 15:12:10 +0200

    feat: add create list buttons

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,56 @@
-import { Anchor, Button, Input, Stack, Text } from "@mantine/core"
-import { useState } from "react"
+import { Button, Group, Stack, Text, TextInput } from "@mantine/core"
+import { useState } from "react";
+import "@mantine/core/styles.css";
+import { ThemeProvider } from "~theme";
 
-import "@mantine/core/styles.css"
-import { ThemeProvider } from "~theme"
+
+function ListEnterItem({ isEnteringName, setIsEnteringListName, setData, data }) {
+  const handleCreateClick = () => setIsEnteringListName(true)
+  const handleCancelClick = () => setIsEnteringListName(false)
+
+  if (!isEnteringName) {
+    return (
+      <Button onClick={handleCreateClick} fullWidth>
+      + Create new list
+      </Button>
+    )
+  }
+  return (
+    <Group>
+      <TextInput
+        size="md"
+        radius="md"
+        placeholder="Enter List Name"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
+      <Button>Create</Button>
+      <Button
+        onClick={handleCancelClick}
+        variant="outline"
+        color="gray">Cancel</Button>
+    </Group>
+  )
+}
+
 
 function IndexPopup() {
   const [data, setData] = useState("")
+  const [isEnteringListName, setIsEnteringListName] = useState(false)
 
   return (
     <ThemeProvider>
-      <Stack miw={240} p="lg">
+      <Stack miw={540} p="lg">
         <Text fw="bold" size="xl">
-          Welcome to your{" "}
-          <Anchor href="https://www.plasmo.com" target="_blank">
-            Plasmo
-          </Anchor>{" "}
-          Extension!
+          FeedBuilder
         </Text>
-        <Input onChange={(e) => setData(e.target.value)} value={data} />
-        <Button component="a" href="https://docs.plasmo.com" target="_blank">
-          View Docs
-        </Button>
+        <ListEnterItem
+          setIsEnteringListName={setIsEnteringListName}
+          isEnteringName={isEnteringListName}
+          setData={setData}
+          data={data}
+        >
+        </ListEnterItem>
       </Stack>
     </ThemeProvider>
   )
